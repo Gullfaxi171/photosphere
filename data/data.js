b49f606d2f64ac0b0f00f6999f8cb7514da6cb6d
Gullfaxi171 <florent.gros@gadz.org>
2014-03-16 21:50:04 +0100

    new spheres

---
+++ data/data.js
@@ -0,0 +1,27 @@
+		// number of division of the sphere. The more, the cooler, the less, the faster
+		var resHD = 50, resSD = 20;
+		
+		var textures = ['photospheres/cluny_cloitre.jpg',
+						  'photospheres/cluny_transept.jpg',
+						  'photospheres/cluny_zaronde.jpg',
+						  'photospheres/cluny_zabels.jpg',
+						  'photospheres/cluny_pips.jpg',
+						  'photospheres/cluny_circuitmh.jpg',
+						  'photospheres/kin_grandecour.jpg',
+						  'photosphere/p3_iena.jpg',
+						  'photosphere/p3_iena_salon_2.jpg',
+						  'photosphere/p3_opera.jpg',
+						  'photosphere/p3_madeleine_2.jpg'
+		];
+		
+		var titres = [
+		
+		];
+		
+		/*var texturesSD = ['photospheres/cluny_cloitre_sd.jpg',
+						  'photospheres/cluny_transept_sd.jpg',
+						  'photospheres/cluny_zaronde_sd.jpg',
+						  'photospheres/cluny_zabels_sd.jpg',
+						  'photospheres/cluny_pips_sd.jpg',
+						  'photospheres/cluny_circuitmh_sd.jpg'
+		];*/
